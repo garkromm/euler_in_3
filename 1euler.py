eded6e154f2165ac289e3504570775214c76d14f
garkromm <garkromm@gmail.com>
2021-05-15 14:30:28 +0300

    pep8 formatting

--- 1euler.py
+++ 1euler.py
@@ -2,7 +2,8 @@
 
 # Multiples of 3 and 5
 #
-# If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9.
+# If we list all the natural numbers below 10 that are multiples of 3 or 5, we
+# get 3, 5, 6 and 9.
 # The sum of these multiples is 23.
 # Find the sum of all the multiples of 3 or 5 below 1000.
 
@@ -30,9 +31,9 @@ def sum_mul_3_5(below):
 
 
 def main():
-    ans = euler_sum_max_div(3, 1000)
-    + euler_sum_max_div(5, 1000)
-    - euler_sum_max_div(15, 1000)
+    ans = (euler_sum_max_div(3, 1000) +
+           euler_sum_max_div(5, 1000) -
+           euler_sum_max_div(15, 1000))
     print(ans)
 
 
